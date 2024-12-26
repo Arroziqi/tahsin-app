8dcd3fbfad3f7783d4966e3acfbfceb9a47fad7d
Arroziqi <ahmadarroziqi@gmail.com>
2024-12-26 21:29:15 +0700

    setup api

--- tahsin-api/src/app.module.ts
+++ tahsin-api/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { CommonModule } from './common/common.module';
+import { FeaturesModule } from './features/features.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    CommonModule,
+    FeaturesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
